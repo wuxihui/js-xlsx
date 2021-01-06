76c7c6ef50645838b111074eeedc85eb486e2aed
wuxihui <2264850089@qq.com>
2021-01-06 15:36:02 +0800

    各种插件

--- src/view/login.vue
+++ src/view/login.vue
@@ -1,6 +1,23 @@
 <template>
 <!-- eslint-disable -->
     <div class="login">
+         <div class="viedo-main">
+            <nav id="wrapper">
+                <ul>
+                    <li><a href="">山楂</a></li>
+                    <li><a href="">梨子</a></li>
+                    <li><a href="">菠萝</a></li>
+                    <li><a href="">香蕉</a></li>
+                    <li><a href="">芒果</a></li>
+                    <li><a href="">猕猴桃</a></li>
+                    <li><a href="">西柚</a></li>
+                    <li><a href="">火龙果</a></li>
+                    <li><a href="">橙子</a></li>
+                </ul>
+            </nav>
+        </div>
+
+
         <h1 @click="handleFooter">底部弹起弹框</h1>
         <van-popup v-model="show" position="bottom" round :style="{ height: '30%' }" />
         <van-button type="primary" @click="handleToRepeat">主要按钮</van-button>
@@ -14,6 +31,16 @@
 
         <button @click="exportData">导出</button>
         <button @click="exportRs">导出Excel报表</button>
+
+        <span class="element"></span> 
+        <!-- <ul>
+            <li class="reveal-top">
+                <img src="http://g.hiphotos.baidu.com/image/pic/item/37d3d539b6003af37401eb21392ac65c1038b633.jpg" class="img">
+            </li>
+            <li class="reveal-top">
+                <img src="http://g.hiphotos.baidu.com/image/pic/item/37d3d539b6003af37401eb21392ac65c1038b633.jpg" class="img">
+            </li>
+        </ul> -->
     </div>
 </template>
 
@@ -23,6 +50,9 @@ import { mapState } from 'vuex'
 import HelloWord from '../components/HelloWorld'
 import XLSX from 'xlsx'
 import FileSaver from 'file-saver'
+import scrollReveal from 'scrollreveal'
+import Iscroll from 'iscroll'
+import Typed from 'typed.js'
 export default {
     name: 'Login',
     data() {
@@ -51,7 +81,8 @@ export default {
             dataArr: [
                 { name: '张三', age: 12, sex: '男', department: 'department1' },
                 { name: '李四', age: 13, sex: '女', department: 'department2' }
-            ]
+            ],
+            scrollReveal: scrollReveal()
         }
     },
     components: {
@@ -62,12 +93,52 @@ export default {
     },
     mounted() {
         console.log(this.loginList);
-
+        // filter方法数组去重
         let myArr = [1,3,4,5,6,3,7,4];
         let newArr = myArr.filter((value,index,arr) => {
             return arr.indexOf(value) == index
         });
         console.log(newArr);
+        //Scroll Reveal js动画效果
+        this.scrollReveal.reveal('.reveal-top', {
+            // 动画的时长
+            duration: 2000,
+            // 延迟时间
+            delay: 500,
+            // 动画开始的位置，'bottom', 'left', 'top', 'right'
+            origin: 'top',
+            // 回滚的时候是否再次触发动画
+            reset: false,
+            // 在移动端是否使用动画
+            mobile: false,
+            // 滚动的距离，单位可以用%，rem等
+            distance: '200px',
+            // 其他可用的动画效果
+            opacity: 0.001,
+            easing: 'linear',
+            scale: 0.9,
+        });
+
+        document.addEventListener("touchmove", function (ev) {
+                ev.preventDefault()
+            }, { passive: false })
+        let iscroll = new Iscroll("#wrapper",{
+            scrollX: true, //横轴滚动
+            scrollY: false, //纵轴false
+            mouseWheel: true, //开启鼠标滚动
+            scrollbars: 'custom',//true, //底部显示滚动条  滚动条样式
+            momentum: true, //在用户快速触摸屏幕时，你可以开/关势能动画。关闭此功能将大幅度提升性能
+            invertWheelDirection: false, //当鼠标滚轮支持激活后，在有些情况下需要反转滚动的方向
+            fadeScrollbars: true, //不想使用滚动条淡入淡出方式时，需要设置此属性为false以便节省资源
+            shrinkScrollbars: 'scale', //clip是移动指示器到它容器的外面，效果就是滚动条收缩起来，简单的移动到屏幕以外的区域。属性设置为此值后将大大的提升整个iScroll的性能
+            snap: 'li',
+        })
+        //酷炫的打字机效果
+        let typed = new Typed('.element', {
+            strings: ["纠正一个错误的方法有很多", "而解决一个错误的终极方法，就是不给它发生的机会。"], //输入内容, 支持html标签
+            typeSpeed: 100, //打字速度
+            backSpeed: 50 //回退速度
+        });
     },
     methods: {
        handleFooter() {
@@ -96,6 +167,16 @@ export default {
             XLSX.writeFile(wb, '数据.xlsx') // 保存的文件名
        },
        exportRs () {
+            // Anime.js 一个JavaScript动画库，可以处理CSS属性，单个CSS转换，SVG或任何DOM属性以及JavaScript对象
+            // Velocity 一个高效的 Javascript 动画引擎，与jQuery的 $.animate() 有相同的API, 同时还支持彩色动画、转换、循环、画架、SVG支持和滚动等效果
+            // Vivus 一个零依赖的JavaScript动画库，可以让我们用SVG制作动画，使其具有被绘制的外观
+            // GreenSock JS 一个JavaScript动画库，用于创建高性能、零依赖、跨浏览器动画，已在超过400万个网站上使用, 并且可以在React、Vue、Angular项目中使用
+            // Scroll Reveal 零依赖，为 web 和移动浏览器提供了简单的滚动动画，以动画的方式显示滚动中的内容
+            // Kute.js 一个强大高性能且可扩展的原生JavaScript动画引擎，具有跨浏览器动画的基本功能
+            // Typed.js 一个轻松实现打字效果的js插件
+            // fullPage.js 一个可轻易创建全屏滚动网站的js滚动动画库, 兼容性无可替代
+            // iscroll 移动端使用的一款轻量级滚动插件
+            //SortableJS 功能强大的JavaScript 拖拽库
             // var param = {}; // 参数
             // let url = '接口地址';
             // axios.post(url, param, {responseType: 'blob'})
@@ -116,4 +197,40 @@ export default {
 
 <style scoped>
 @import '../styles/login.css';
+nav{
+    width: 100%;
+    height: 2rem;
+    line-height: 2rem;   
+    background-color: green;
+    overflow: auto;
+}
+nav ul {
+    width: 150%;
+    display: flex;
+    align-items: center;
+}
+nav ul li {
+    flex: 1;
+}
+/* .typed-cursor{
+    opacity: 1;
+    -webkit-animation: blink 0.7s infinite;
+    -moz-animation: blink 0.7s infinite;
+    animation: blink 0.7s infinite;
+}
+@keyframes blink{
+    0% { opacity:1; }
+    50% { opacity:0; }
+    100% { opacity:1; }
+}
+@-webkit-keyframes blink{
+    0% { opacity:1; }
+    50% { opacity:0; }
+    100% { opacity:1; }
+}
+@-moz-keyframes blink{
+    0% { opacity:1; }
+    50% { opacity:0; }
+    100% { opacity:1; }
+} */
 </style>
--- src/view/scrollbar.vue
+++ src/view/scrollbar.vue
@@ -1,15 +1,32 @@
 <template>
 <!-- eslint-disable -->
     <div>
-        <ul>
+        <!-- <ul>
             <li v-for="item in 100" :key="item" @click="handleDetails(item)">
                 {{item}}
             </li>
-        </ul>
+        </ul> -->
+
+      <div class="container" style="height: 520px">
+			<div data-force="30" class="layer block" style="left: 14.5%; top: 0; width: 37%">
+				<div class="layer title">List A</div>
+				<ul id="foo" class="block__list block__list_words">
+					<li>1aaaбегемот测试</li>
+					<li>2корм</li>
+					<li>3антон</li>
+					<li>4сало</li> 
+					<li>5железосталь</li>
+					<li>6валик</li>
+					<li>7кровать</li>
+					<li>8краб</li>
+				</ul>
+			</div>
+		</div>
     </div>
 </template>
 <!-- eslint-disable -->
 <script>
+import Sortable from 'sortablejs'
 export default {
     name: 'scrollbar',
     data() {
@@ -18,7 +35,51 @@ export default {
         }
     },
     activated () {
+        // html2canvas 一个强大的使用js开发的浏览器网页截图工具
+        // dom-to-image 一个可以将任意DOM节点转换为用JavaScript编写的矢量（SVG）或光栅（PNG或JPEG）图像的库
+        // pica 一个在浏览器中调整图像大小，而不会出现像素失真，处理速度非常快的图片处理库
+        // Lena.js 一个轻量级的可以给你图像加各种滤镜的js库
+        // Compressor.js 一个使用本地canvas.toBlob API进行图像有损压缩的js库
+        // Fabric.js 一个易于使用的基于HTML5 canvas元素的图片编辑器
+        // merge-images 一个将多张图片合并成一张图的js插件
+        // cropperjs 一款强大的图片裁切库, 支持灵活的图片裁切方式
+        // Grade 一个基于图像中的前2种主要颜色生成互补渐变背景的库
+        //Validator.js 一个强大的js表单校验库, github地址: https://github.com/validatorjs/validator.js
+        //Validate.js 致力于提供一种验证数据的跨框架和跨语言方式的js库, 已通过100％代码覆盖率的单元测试 github地址: https://github.com/ansman/validate.js
+        //x-spreadsheet 一个基于web的简单易用的表格插件 github: https://github.com/myliang/x-spreadsheet
+
         console.log("activated");
+        Sortable.create(document.getElementById('foo'), {
+				animation: 150,
+				store: {//缓存到localStorage
+					get: function(sortable) {
+						var order = localStorage.getItem(sortable.options.group);
+						return order ? order.split('|') : [];
+					},
+					set: function(sortable) {
+						var order = sortable.toArray();
+						localStorage.setItem(sortable.options.group, order.join('|'));
+					}
+				},
+				onAdd: function(evt) {
+					console.log('onAdd.foo:', [evt.item, evt.from]);
+				},
+				onUpdate: function(evt) {
+					console.log('onUpdate.foo:', [evt.item, evt.from]);
+				},
+				onRemove: function(evt) {
+					console.log('onRemove.foo:', [evt.item, evt.from]);
+				},
+				onStart: function(evt) {
+					console.log('onStart.foo:', [evt.item, evt.from]);
+				},
+				onSort: function(evt) {
+					console.log('onStart.foo:', [evt.item, evt.from]);
+				},
+				onEnd: function(evt) {
+					console.log('onEnd.foo:', [evt.item, evt.from]);
+				}
+			});
     },
     deactivated () {
         console.log("deactivated");
@@ -37,7 +98,7 @@ export default {
 
 <style scoped>
 li {
-    width: 100%;
+    width: 150px;
     height: 25px;
     background: green;
     margin: 5px 0;
